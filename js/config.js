be8081249122f22088a665a4236d1986103b5eb3
NicolasRobertson <46912440+NicolasRobertson@users.noreply.github.com>
2021-04-01 18:33:57 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_po9XI66fv', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-east-1_po9Xl66fv', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '18taa4p6o18kb4fdtqmhamv91h', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
